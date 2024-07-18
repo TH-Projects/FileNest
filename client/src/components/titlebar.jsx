25984391a9eaf3b3ee107fcb2547d9670551f272
TH-Projects <mail@timoheckmann.de>
2024-07-18 15:02:49 +0200

    Fixed wrong space in titlebar

--- client/src/components/titlebar.jsx
+++ client/src/components/titlebar.jsx
@@ -1,22 +1,24 @@
-import Navbar from 'react-bootstrap/Navbar'
-import Container from 'react-bootstrap/Container'
-import Image from 'react-bootstrap/Image'
-import Button from 'react-bootstrap/Button'
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import Image from "react-bootstrap/Image";
+import Button from "react-bootstrap/Button";
 
 const Titlebar = () => {
-    return (
-        <Navbar className="bg-dark navbar-dark">
-          <Container className="justify-content-between">
-            <Navbar.Brand>
-                <Image src='logo.png' fluid rounded style={{ maxHeight: '90px'}} />
-            </Navbar.Brand>
-            <Navbar.Text className="text-center">
-                <h5 className='display-6 text-white'>FileNest - Dashboard</h5>
-            </Navbar.Text>
-            <Button variant="outline-info" size='lg'>Log In</Button>
-          </Container>
-        </Navbar>
-      );
-}
+  return (
+    <Navbar className="bg-dark navbar-dark">
+      <Container fluid style={{ marginLeft: "30px", marginRight: "30px" }}>
+        <Navbar.Brand>
+          <Image src="logo.png" rounded style={{ maxHeight: "90px" }} />
+        </Navbar.Brand>
+        <Navbar.Text className="text-center">
+          <h5 className="display-6 text-white">FileNest - Dashboard</h5>
+        </Navbar.Text>
+        <Button variant="outline-info" size="lg">
+          Log In
+        </Button>
+      </Container>
+    </Navbar>
+  );
+};
 
 export default Titlebar;
