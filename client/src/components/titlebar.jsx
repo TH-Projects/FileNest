ff39398910987d5cdce77662173a271277b9a62d
Timo Heckmann <mail@timoheckmann.de>
2024-07-25 20:58:37 +0200

    Added home navigation for logo and title

--- client/src/components/titlebar.jsx
+++ client/src/components/titlebar.jsx
@@ -20,11 +20,11 @@ const Titlebar = () => {
   return (
     <Navbar className="bg-dark navbar-dark">
       <Container fluid style={{ marginLeft: '50px', marginRight: '50px' }}>
-        <Navbar.Brand>
+        <Navbar.Brand onClick={()=>navigate('/')}>
           <Image src="logo.png" rounded style={{ maxHeight: '90px' }} />
         </Navbar.Brand>
         <Navbar.Text className="text-center">
-          <h5 className="display-6 text-white">FileNest - Dashboard</h5>
+          <h5 className="display-6 text-white" onClick={()=>navigate('/')}>FileNest - Dashboard</h5>
         </Navbar.Text>
         <Button variant="outline-info" size="lg" onClick={handleButtonClick}>
           {user ? 'Abmelden' : 'Log In'}
