10307c63f6426940e2b968a655dc30e3ea944164
Timo Heckmann <mail@timoheckmann.de>
2024-08-04 21:49:36 +0200

    Added file download from minIO

--- client/src/components/view-file.jsx
+++ client/src/components/view-file.jsx
@@ -1,6 +1,8 @@
 /* eslint-disable react/prop-types */
 import { Container, Row, Col, Button } from "react-bootstrap";
 import { useAuth } from "../contextes/auth-context";
+
+import { saveAs } from 'file-saver';
 import "../style/cards.css";
 
 // Utility function to format a timestamp
@@ -18,7 +20,30 @@ const formatTimestamp = (timestamp) => {
 };
 
 const FileView = ({ file_meta_data }) => {
-  const {user} = useAuth();
+  const { user } = useAuth();
+
+  const handleDownload = async (filename) => {
+    try {
+      const response = await fetch(`http://localhost:3000/download/${filename}`, {
+        method: 'GET',
+        headers: {
+          'Accept': 'application/octet-stream',
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+
+      const blob = await response.blob();
+      saveAs(blob, filename); // Use the dynamic file name
+    } catch (error) {
+      console.error('Error downloading the file:', error);
+      alert('Fehler beim Herunterladen der Datei');
+    }
+  };
+
+
   return (
     <Container fluid>
       <Row className="table-row align-items-center r-h-3">
@@ -28,7 +53,12 @@ const FileView = ({ file_meta_data }) => {
         <Col md={2}>{file_meta_data.owner}</Col>
         <Col md={2}>{formatTimestamp(file_meta_data.lastModified)}</Col>
         <Col md={2} className="d-flex justify-content-end">
-          <Button variant="success" className="w-40 me-2" disabled={!user}>
+          <Button
+            variant="success"
+            className="w-40 me-2"
+            disabled={!user}
+            onClick={() => handleDownload(`${file_meta_data.name}.${file_meta_data.extension}`)}
+          >
             Download
           </Button>
           <Button variant="danger" className="w-40" disabled={!user}>
